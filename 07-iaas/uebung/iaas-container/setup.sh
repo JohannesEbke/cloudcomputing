5f7250e1a3a7a652e932120965ae817a964cb5fa
Andreas Kowasch <andreas.kowasch@qaware.de>
2024-05-22 18:31:05 +0200

    Refresh iaas exercises

--- 07-iaas/uebung/iaas-container/setup.sh
+++ 07-iaas/uebung/iaas-container/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
+# Install required packages
+
 apt-get update
 apt-get install -y -q \
   gnupg \
@@ -12,26 +14,30 @@ apt-get install -y -q \
   less \
   lsb-release
 
+################################################################################
+
+# Install Terraform CLI
+
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list
+
 apt-get update
 apt-get install -y -q terraform
 rm -rf /var/lib/apt/lists/*
 
-mkdir bin
-cd bin
+################################################################################
+
+# Install AWS CLI
+
+mkdir bin && cd "$_"
 
 case $(uname -m) in
   aarch64)
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
-    ;;
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip" ;;
   x86_64)
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    ;;
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" ;;
   *)
-    echo "Unsupported architecture"
-    exit 1
-    ;;
+    echo "Unsupported architecture" && exit 1 ;;
 esac
 unzip awscliv2.zip
 rm awscliv2.zip
